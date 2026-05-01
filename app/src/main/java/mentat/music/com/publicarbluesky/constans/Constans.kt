c79ebd8707f522aa6287ddc707d17249634dca02
Juan Mentat <31702276+jordiguzman@users.noreply.github.com>
2026-05-01 19:29:12 +0200

    Remove Constants object from Constans.kt

--- app/src/main/java/mentat/music/com/publicarbluesky/constans/Constans.kt
+++ app/src/main/java/mentat/music/com/publicarbluesky/constans/Constans.kt
@@ -1,15 +1,1 @@
-package mentat.music.com.publicarbluesky.constans
 
- object Constants{
-
-
-
-         const val BLUESKY_USERNAME = "juanmentat.bsky.social"
-         const val BLUESKY_APP_PASSWORD = "xxxx-xxxx-xxxx-xxxx"
-
-         const val BASE_URL_HUBBLE = "https://esahubble.org/images/viewall/"
-
-         const val BASE_URL_IMAGE  ="http://cdn.esahubble.org/archives/images/screen/"
-
-
-}
